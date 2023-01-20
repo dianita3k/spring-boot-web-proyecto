e4e3c1662d32be19e7dd106e36545f518ed3d1e7
Diana Zhizhin <dzhizhin632@gmail.com>
2023-01-20 09:46:11 -0500

    Initial commit

---
+++ src/test/java/com/ista/examen/web/app/SpringBootWebProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ista.examen.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
